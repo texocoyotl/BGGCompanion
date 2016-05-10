b0fecc863144d36e5a2019f9edb6407e59e9af31
Benjamin Molina <gaeldron.might@gmail.com>
2016-05-09 20:40:52 -0400

    Added Share fab to detail

--- app/src/main/java/com/texocoyotl/bggcompanion/detail/DetailActivity.java
+++ app/src/main/java/com/texocoyotl/bggcompanion/detail/DetailActivity.java
@@ -1,5 +1,7 @@
 package com.texocoyotl.bggcompanion.detail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +13,8 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.texocoyotl.bggcompanion.R;
+import com.texocoyotl.bggcompanion.database.Contract;
+import com.texocoyotl.bggcompanion.hotlist.HotListActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,32 +27,52 @@ public class DetailActivity extends AppCompatActivity implements DetailActivityF
     @BindView(R.id.collapsing_toolbar)
     CollapsingToolbarLayout mCollapsingToolBar;
 
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    private String mBGGID;
+    private String mTitle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
 
+        Uri mDetailUri = getIntent().getParcelableExtra(HotListActivity.BUNDLE_KEY_DETAIL_URI);
+        mBGGID = Contract.BoardgameEntry.getIDFromUri(mDetailUri);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
     public void loadHeader(String url, String title) {
         Glide.with(this).load(url).into(mImageView);
+        mTitle = title;
         mCollapsingToolBar.setTitle(title);
         mCollapsingToolBar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(createShareIntent());
+            }
+        });
+
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+
+        String text = getString(R.string.share_text, mTitle, mBGGID);
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+        return shareIntent;
     }
 }
